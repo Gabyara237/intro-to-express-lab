f804ba17b56e4f07d3c9d6d1a3648d4dbaa7d307
Gabriela Araujo <gabyara237@gmail.com>
2025-12-01 20:11:10 -0800

    Add solution for exercise 3: collectible route

--- server.js
+++ server.js
@@ -22,6 +22,29 @@ app.get('/roll/:number', (req,res)=>{
 
 })
 
+// Exercise 3. I Want THAT One!
+
+  const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+app.get('/collectibles/:index', (req, res) => {
+
+    const idx = req.params.index;
+
+    if (idx >= collectibles.length){
+        res.send("This item is not yet in stock. Check back soon!")
+    }else{
+        res.send(`So, you want the ${collectibles[idx].name}? For ${collectibles[idx].price}, it can be yours!`)
+    }
+
+})
+
+
 app.listen(3000,()=>{
     console.log('Listening on port 3000')
 })
+
+
