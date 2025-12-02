c3974a38f7d3ffcbea168dacc3156aaf3e043eab
Gabriela Araujo <gabyara237@gmail.com>
2025-12-01 19:02:59 -0800

    Add solution for exercise 2: roll route

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.get('/greetings/:username', (req,res)=>{
     res.send(`<h1>Hello there, ${req.params.username}! </h1>`)
 })
 
+// Exercise 2. Rolling the Dice
+
+app.get('/roll/:number', (req,res)=>{
+
+    const param = req.params.number;
+    
+    if (!isNaN(param)){
+        res.send(`You rolled a ${Math.floor(Math.random(0,parseInt(param)) * parseInt(param)+1)}`)
+    }else{
+        res.send('You must specify a number.')
+    }
+
+})
+
 app.listen(3000,()=>{
     console.log('Listening on port 3000')
 })
